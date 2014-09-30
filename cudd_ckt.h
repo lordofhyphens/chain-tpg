466cc6eb0c830d09f4b716b57b20a7bace432305
Joseph Lenox <lenox.joseph@gmail.com>
2014-09-30 16:51:31 -0500

    Fixed a bug in ckt_cudd.h

--- cudd_ckt.h
+++ cudd_ckt.h
@@ -14,32 +14,36 @@ class CUDD_Circuit : public Circuit {
     std::map<int, BDD> po;
     std::map<int, BDD> dff; // internals flipflops
     std::map<int, BDD> net; // all internal netlist bdds generated.
+    std::map<int, BDD> pi;
     CUDD_Circuit(Cudd manager) : Circuit(),  _manager(manager) { };
     CUDD_Circuit() : Circuit() { 
       _manager = Cudd(0,0);
     };
+    Cudd getManager() { return _manager; }
     void form_bdds()
     {
       for (std::vector<NODEC>::iterator gate = graph->begin(); gate < graph->end(); gate++)
       {
         const int pos = gate - graph->begin();
+        BDD result;
         std::cerr << "Working on gate " << pos << ", " << gate->name<< "\n";
         switch(gate->typ)
         {
           case DFF:
           case INPT:
-            net[pos] = _manager.bddVar(pos);
+            result  = _manager.bddVar(pos);
+            pi[pos] = result;
             break;
           case NOT:
-            net[pos] = !net[gate->fin[0].second];
+            result = !net[gate->fin[0].second];
             break;
           case FROM:
-            net[pos] = net[gate->fin[0].second];
+            result = net[gate->fin[0].second];
             break;
           default:
             fin_t::iterator fin = gate->fin.begin();
             std::cerr << "\tWorking on fanin " << fin->second << ", " << fin->first<< "\n";
-            BDD result = net[fin->second];
+            result = net[fin->second];
             fin++;
             // Make the BDD from the fanins.
             for (; fin < gate->fin.end(); fin++) {
@@ -61,12 +65,13 @@ class CUDD_Circuit : public Circuit {
               }
               break;
             }
-            net[pos] = result;
-            if (gate->typ == DFF_IN)
-              dff[pos] = result;
-            if (gate->po)
-              po[pos] = result;
+
         }
+        net[pos] = result;
+        if (gate->typ == DFF_IN)
+          dff[pos] = result;
+        if (gate->po && gate->typ != DFF_IN)
+          po[pos] = result;
       }
       // Don't need the intermediate gate node BDDs, so clear them so 
       // garbage collection can happen.
