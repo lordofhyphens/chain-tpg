d0c3d4f6ea69abb8befe487c6c0409ac56792b21
Joseph Lenox <lenox.joseph@gmail.com>
2015-05-27 22:01:57 -0500

    merged Jonas' tests and added more for bdd_img

--- tests/bdd_img_test.cpp
+++ tests/bdd_img_test.cpp
@@ -1,12 +1,44 @@
 #include "../bdd_img.h"
+#include "../cudd_ckt.h"
+#include "../getpis.h"
+#include "../bdd_util.h"
 #include "CppUTest/TestHarness.h"
 #include "CppUTest/TestOutput.h"
+
+using unique_ckt = std::unique_ptr<CUDD_Circuit>;
+using imgcache_t = std::map<BDD_map_pair, BDD>;
+TEST_GROUP(Constant_Input){
+  Cudd* manager;
+  std::map<int, BDD> vars;
+  std::map<int, int> mapping;
+  std::map<int, BDD> funcs;
+  imgcache_t cache;
+
+  void setup(){
+    manager = new Cudd();
+    vars[0] = BDD(manager->bddOne());
+
+    mapping[0] = 0;
+
+    funcs[0] = vars[0];
+  }
+
+    void teardown(){
+      vars.clear();
+      funcs.clear();
+      cache.clear();
+      delete manager;
+    }
+
+};
+
+
 TEST_GROUP(Sandbox){
   Cudd* manager;
   std::map<int, BDD> vars;
   std::map<int, int> mapping;
   std::map<int, BDD> funcs;
-  std::map<BDD_map_pair, BDD> cache;
+  imgcache_t cache;
 
   void setup(){
   manager = new Cudd();
@@ -28,13 +60,35 @@ TEST_GROUP(Sandbox){
   delete manager;
   }
 };
+TEST(Sandbox, ComplementaryComponents00){
+  BDD prev = !vars[0] * !vars[1];
+  BDD result = img(funcs, mapping, prev, *manager, cache);
+  DOUBLES_EQUAL(1, result.CountMinterm(funcs.size()), 0.1);
+}
+TEST(Sandbox, ComplementaryComponents01){
+  BDD prev = !vars[0] * vars[1];
+  BDD result = img(funcs, mapping, prev, *manager, cache);
+  DOUBLES_EQUAL(1, result.CountMinterm(funcs.size()), 0.1);
+}
+
+TEST(Sandbox, ComplementaryComponents10){
+  BDD prev = vars[0] * !vars[1];
+  BDD result = img(funcs, mapping, prev, *manager, cache);
+  DOUBLES_EQUAL(1, result.CountMinterm(funcs.size()), 0.1);
+}
+
+TEST(Sandbox, ComplementaryComponents11){
+  BDD prev = vars[0] * vars[1];
+  BDD result = img(funcs, mapping, prev, *manager, cache);
+  DOUBLES_EQUAL(1, result.CountMinterm(funcs.size()), 0.1);
+}
 TEST_GROUP(BDD_Img_Toy)
 {
   Cudd* manager;
   std::map<int, BDD> vars;
   std::map<int, int> mapping;
   std::map<int, BDD> funcs;
-  std::map<BDD_map_pair, BDD> cache;
+  imgcache_t cache;
 
   void setup()
   {
@@ -65,18 +119,35 @@ TEST_GROUP(BDD_Img_Toy)
     delete manager;
   }
 };
-TEST(BDDIMG_s27, ConstrainCheck)
-{
-  BDD prev = ckt->get_minterm_from_string("-------0-1010010000-010101--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-  BDD next_expected = ckt->get_minterm_from_string("-------1-1011000100-000010--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-  BDD next_all = img(ckt->dff, ckt->dff_pair, ckt->getManager(), cache);
-  BDD next_const = img(ckt->dff, ckt->dff_pair, prev, ckt->getManager(), cache);
-  BDD next_all_clean = RemoveInvalidMintermFromImage(*ckt, prev, next_const);
-  CHECK(next_all_clean == next_const);
+TEST_GROUP(Identical_Components){
+  Cudd* manager;
+  std::map<int, BDD> vars;
+  std::map<int, int> mapping;
+  std::map<int, BDD> funcs;
+  std::map<int, BDD> funcs2;
+  imgcache_t cache;
 
+  void setup(){
+  manager = new Cudd();
+  vars[0] = BDD(manager->bddVar(0));  //a DFF
+  vars[1] = BDD(manager->bddVar(1));  //b DFF
+  vars[2] = BDD(manager->bddVar(2));  //c DFF
 
-  CHECK_TEXT(next_all != next_const, "Constrained img should not equal unconstrained img");
+  for (int i = 0; i < 1; i++){
+    mapping[i] = i;
+  }
+
+  funcs[0] = vars[0]*(vars[1] + vars[2]);
+  funcs2[0] = vars[0]*(vars[1] + vars[2]);
 }
+void teardown(){
+  vars.clear();
+  funcs.clear();
+  funcs2.clear();
+  cache.clear();
+  delete manager;
+}
+};
 TEST(Identical_Components, Prev000){
   BDD prev = !vars[0] * !vars[1] * !vars[2];
   BDD result = img(funcs, mapping, prev, *manager, cache);
@@ -89,36 +160,14 @@ TEST(Identical_Components, Prev000){
   // result.PrintCover();
   CHECK_TRUE(result == result2);
 }
-TEST(Sandbox, ComplementaryComponents00){
-  BDD prev = !vars[0] * !vars[1];
-  BDD result = img(funcs, mapping, prev, *manager, cache);
-  DOUBLES_EQUAL(1, result.CountMinterm(funcs.size()), 0.1);
-}
-TEST(BDDIMG_s27, RandomCheck)
-{
-  std::cerr << "\n";
-  for (int i = 0; i < 32; i++) {
-    BDD prev = img(ckt->dff, ckt->dff_pair,ckt->getManager(), cache).PickOneMinterm(ckt->dff_vars);
-    BDD next_all = img(ckt->dff, ckt->dff_pair, prev, ckt->getManager(), cache) - prev;
-    size_t count = next_all.CountMinterm(ckt->dff.size());
-    verbose_flag =1;
-    BDD next_all_clean = RemoveInvalidMintermFromImage(*ckt, prev, next_all);
-    verbose_flag =0;
 
-    prev.PickOneMinterm(ckt->dff_vars).PrintCover();
-    next_all_clean.PickOneMinterm(ckt->dff_vars).PrintCover();
-
-//    BDD next_all_clean = next_all;
-    size_t cleancount = next_all_clean.CountMinterm(ckt->dff.size());
-    std::cerr << "next minterm " << count << "," << cleancount << " (" <<  next_all.CountMinterm(ckt->dff.size()) << ")" << "\n";
-    CHECK_EQUAL(cleancount, count);
-  }
-}
-TEST(Sandbox, ComplementaryComponents01){
-  BDD prev = !vars[0] * vars[1];
+TEST(Identical_Components, Prev001){
+  BDD prev = !vars[0] * !vars[1] * vars[2];
   BDD result = img(funcs, mapping, prev, *manager, cache);
-  DOUBLES_EQUAL(1, result.CountMinterm(funcs.size()), 0.1);
+  BDD result2 = img(funcs2, mapping, prev, *manager, cache);
+  CHECK_TRUE(result == result2);
 }
+
 TEST(Identical_Components, Prev010){
   BDD prev = !vars[0] * vars[1] * !vars[2];
   BDD result = img(funcs, mapping, prev, *manager, cache);
@@ -160,13 +209,209 @@ TEST(Identical_Components, Prev111){
   BDD result2 = img(funcs2, mapping, prev, *manager, cache);
   CHECK_TRUE(result == result2);
 }
+TEST_GROUP(C17_Img)
+{
+  Cudd* manager;
+  std::map<int,BDD> vars;
+  std::map<int,int> mapping;
+  std::map<int,BDD> funcs;
+  imgcache_t cache;
+
+  void setup()
+  {
+    // set up the test BDDs.
+    // the circuit is a variant of c17, with
+    // two of the inputs replaced with copies
+    // of the outputs
+    // 11->11
+    // 10->10, 11, 01
+    // 00->01,
+    manager = new Cudd();
+    vars[0] = BDD(manager->bddVar(0)); // a
+    vars[1] = BDD(manager->bddVar(1)); // b -> DFF
+    vars[2] = BDD(manager->bddVar(2)); // c
+    vars[3] = BDD(manager->bddVar(3)); // d -> DFF
+    vars[4] = BDD(manager->bddVar(4)); // e
+
+    for (int i = 0; i < 5; i++)
+      mapping[i] = i;
+
+    funcs[3] = ~(~(vars[2]*~(vars[1]*vars[3]))*~(vars[0]*vars[1]));
+    funcs[1] = ~(~(vars[4]*~(vars[1]*vars[3]))*~(vars[2]*~(vars[1]*vars[2])));
+
+  }
+  void teardown()
+  {
+    vars.clear();
+    mapping.clear();
+    cache.clear();
+    funcs.clear();
+    delete manager;
+  }
+
+};
+
+TEST(C17_Img, NonZeroNodeCounts)
+{
+  BDD prev = ~vars[1] * ~vars[3];
+  BDD result = img(funcs, mapping, prev, *manager, cache);
+  CHECK(result.CountMinterm(vars.size()) > 0);
+}
+
+TEST(C17_Img, ImgSize10Prev){
+  BDD prev = vars[1] * ~vars[3];
+  BDD result = img(funcs, mapping, prev, *manager, cache);
+  //printf("\n%d\n", result.nodeCount());
+  //result.PrintCover();
+  DOUBLES_EQUAL(2, result.CountMinterm(funcs.size()), 0.1);
+}
+
+TEST(C17_Img, EqualityOperatorOnEqualSizeImgs)
+{
+  BDD prev = vars[1] * vars[3];
+  BDD img1 = img(funcs, mapping, prev, *manager, cache);
+  BDD img2 = img(funcs, mapping, prev, *manager, cache);
+  CHECK_TRUE(img1 == img2);
+}
+
+TEST(C17_Img, EqualityOperatorOnDiffSizeImgs)
+{
+  BDD prev_11 = vars[1] * vars[3];
+  BDD prev_10 = vars[1] * ~vars[3];
+  BDD img_11 = img(funcs, mapping, prev_11, *manager, cache);
+  BDD img_10 = img(funcs, mapping, prev_10, *manager, cache);
+  CHECK_FALSE(img_11 == img_10);
+}
+
+
+TEST(C17_Img, Sandbox)
+{
+  //BDD prev = vars[1] * ~vars[3];
+  //printf("Funcs.size() = %i", funcs.size());
+  CHECK_TRUE(1);
+}
+
+TEST_GROUP(BDDIMG_B14)
+{
+  unique_ckt ckt = nullptr;
+  imgcache_t cache; 
+  void setup() 
+  {
+    ckt = unique_ckt(new CUDD_Circuit());
+    ckt->read_blif("tests/b14.blif");
+    ckt->form_bdds();
+  }
+  void teardown()
+  {
+    ckt = nullptr;
+  }
+};
+TEST(BDDIMG_B14, ConstrainCheck)
+{
+  BDD prev = ckt->get_minterm_from_string("--010100100-001001101110");
+  BDD next_expected = ckt->get_minterm_from_string("--000010100-001101010000");
+  BDD next_all = img(ckt->dff, ckt->dff_pair, ckt->getManager(), cache);
+  BDD next_const = img(ckt->dff, ckt->dff_pair, prev, ckt->getManager(), cache);
+
+  CHECK_TEXT(next_all != next_const, "Constrained img should not equal unconstrained img");
+}
+TEST(BDDIMG_B14, RandomCheck)
+{
+    BDD all_img = img(ckt->dff, ckt->dff_pair, ckt->getManager(), cache);
+    all_img.PickOneMinterm(ckt->dff_vars).PrintCover();
+    BDD prev = ckt->get_minterm_from_string("--010100100-001001101110");
+
+    BDD next_expected = ckt->get_minterm_from_string("--000010100-001101010000");
+    BDD next_img = img(ckt->dff, ckt->dff_pair, prev, ckt->getManager(), cache);
+    BDD tmp = next_img;
+
+    CHECK_FALSE(all_img == next_img);
+    CHECK_FALSE(GetPIs(ckt->getManager(), ckt->dff_io, prev,next_expected).IsZero());
+    CHECK_FALSE((next_expected * next_img).IsZero());
+}
+TEST_GROUP(BDDIMG_s444)
+{
+  unique_ckt ckt = nullptr;
+  imgcache_t cache; 
+  void setup() 
+  {
+    ckt = unique_ckt(new CUDD_Circuit());
+    ckt->read_blif("tests/s444.blif");
+    ckt->form_bdds();
+  }
+  void teardown()
+  {
+    ckt = nullptr;
+  }
+};
+TEST(BDDIMG_s444, ConstrainCheck)
+{
+  BDD prev = ckt->get_minterm_from_string("--010100100-001001101110");
+  BDD next_expected = ckt->get_minterm_from_string("--000010100-001101010000");
+  BDD next_all = img(ckt->dff, ckt->dff_pair, ckt->getManager(), cache);
+  BDD next_const = img(ckt->dff, ckt->dff_pair, prev, ckt->getManager(), cache);
+
+  CHECK_TEXT(next_all != next_const, "Constrained img should not equal unconstrained img");
+}
+TEST(BDDIMG_s444, RandomCheck)
+{
+    BDD prev = ckt->get_minterm_from_string("--010100100-001001101110");
+    BDD next_expected = ckt->get_minterm_from_string("--000010100-001101010000");
+    BDD next_img = img(ckt->dff, ckt->dff_pair, prev, ckt->getManager(), cache);
+    BDD all_img = img(ckt->dff, ckt->dff_pair, ckt->getManager(), cache);
+    BDD tmp = next_img;
+
+    CHECK_FALSE(all_img == next_img);
+    CHECK_FALSE(GetPIs(ckt->getManager(), ckt->dff_io, prev,next_expected).IsZero());
+    CHECK_FALSE((next_expected * next_img).IsZero());
+}
+
+TEST_GROUP(BDDIMG_b13)
+{
+  unique_ckt ckt = nullptr;
+  imgcache_t cache; 
+  void setup() 
+  {
+    ckt = unique_ckt(new CUDD_Circuit());
+    ckt->read_blif("tests/b13.blif");
+    ckt->form_bdds();
+  }
+  void teardown()
+  {
+    ckt = nullptr;
+  }
+};
+TEST(BDDIMG_b13, ConstrainCheck)
+{
+  BDD prev = ckt->get_minterm_from_string("0101001000--------0--101011100000000000010101111011000000100100");
+  BDD next_expected = ckt->get_minterm_from_string("0001010111--------0--000000000001010111101100000010010001001101");
+  BDD next_all = img(ckt->dff, ckt->dff_pair, ckt->getManager(), cache);
+  BDD next_const = img(ckt->dff, ckt->dff_pair, prev, ckt->getManager(), cache);
+
+  CHECK_TEXT(next_all != next_const, "Constrained img should not equal unconstrained img");
+}
+TEST(BDDIMG_b13, RandomCheck)
+{
+
+    BDD prev = ckt->get_minterm_from_string("0101001000--------0--101011100000000000010101111011000000100100");
+    BDD next_expected = ckt->get_minterm_from_string("0001010111--------0--000000000001010111101100000010010001001101");
+    BDD next_img = img(ckt->dff, ckt->dff_pair, prev, ckt->getManager(), cache);
+    BDD all_img = img(ckt->dff, ckt->dff_pair, ckt->getManager(), cache);
+
+    all_img.PickOneMinterm(ckt->dff_vars).PrintCover();
+    CHECK_FALSE(all_img == next_img);
+    CHECK_FALSE(GetPIs(ckt->getManager(), ckt->dff_io, prev,next_expected).IsZero());
+    CHECK_FALSE((next_expected * next_img).IsZero());
+}
+
+
 TEST_GROUP(BDD_Img)
 {
   Cudd* manager;
   std::map<int,BDD> vars;
   std::map<int,int> mapping;
   std::map<int,BDD> funcs;
-  std::map<BDD_map_pair, BDD> cache;
+  imgcache_t cache;
   std::vector<BDD> varlist;
 
   void setup()
