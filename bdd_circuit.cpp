75075fa0b8bd636c7630e847c377b8c6678ad4b6
Joseph Lenox <lenox.joseph@gmail.com>
2015-06-14 02:10:58 -0500

    rewrote to_bdd(), cleaned it up quite a bit.

--- bdd_circuit.cpp
+++ bdd_circuit.cpp
@@ -12,67 +12,60 @@ void BDDCircuit::to_bdd()
   // start making BDDs.
   for (size_t i = 0; i < netlist.size(); i++)
   {
-    if (netlist.at(i).fin.size() == 1)
+    const auto& node = netlist.at(i);
+    BDD result;
+    if(node.type == LogicType::Input)
     {
-      auto it = std::find(netlist.begin(), netlist.end(), netlist.at(i).fin.at(0));
-      assert (it != netlist.end());
-      if (netlist.at(i).type == LogicType::Not)
-        bdd_netlist.emplace_back(!bdd_netlist.at(std::distance(netlist.begin(), it)));
-      else
-        bdd_netlist.emplace_back(bdd_netlist.at(std::distance(netlist.begin(), it)));
+      result = manager.bddVar(i);
+      manager.bddSetPiVar(i);
+      bdd_netlist.emplace_back(std::move(result));
       continue;
     }
-    switch(netlist.at(i).type)
+    if(node.type == LogicType::DFF)
     {
-      case LogicType::DFF:
-      case LogicType::Input:
-        bdd_netlist.emplace_back(manager.bddVar());
-        if (netlist.at(i).type == LogicType::DFF)
-          manager.bddSetNsVar(manager.ReadSize()-1);
-        else
-          manager.bddSetPiVar(manager.ReadSize()-1);
-        break;
-      case LogicType::Unknown: break;
-      default: 
-        {
-          auto& gate = netlist.at(i);
-          assert(gate.fin.size() > 0);
-          auto fin = gate.fin.begin();
-          auto it = std::find(netlist.begin(), netlist.end(), *fin);
-          BDD result = bdd_netlist.at(std::distance(netlist.begin(), it));
-          fin++;
-          for (; fin < gate.fin.end(); fin++) 
-          {
-            it = std::find(netlist.begin(), netlist.end(), *fin);
-            assert(it != netlist.end());
-            auto pos = std::distance(netlist.begin(), it);
-            switch(netlist.at(i).type)
-            {
-              case LogicType::And:
-                result = result.And(bdd_netlist.at(pos));
-                break;
-              case LogicType::Nand:
-                result = result.Nand(bdd_netlist.at(pos));
-                break;
-              case LogicType::Or:
-                result = result.Or(bdd_netlist.at(pos));
-                break;
-
-              case LogicType::Nor:
-                result = result.Nor(bdd_netlist.at(pos));
-                break;
-              case LogicType::Xor:
-                result = result.Xor(bdd_netlist.at(pos));
-                break;
-              case LogicType::Xnor:
-                result = result.Xnor(bdd_netlist.at(pos));
-                break;
-              default: assert(netlist.at(i).type != LogicType::Unknown);
-            }
-            bdd_netlist.emplace_back(std::move(result));
-          }
-        }
+      result = manager.bddVar(i);
+      manager.bddSetNsVar(i);
+      bdd_netlist.emplace_back(std::move(result));
+      continue;
     }
+    // catch everything else
+    assert(node.nfi() > 0);
+    auto pos = std::distance(netlist.cbegin(), std::find(netlist.cbegin(), netlist.cend(), node.fin.at(0)));
+    result = bdd_netlist.at(pos);
+    switch(node.type)
+    {
+      case LogicType::Buff:
+      case LogicType::DFF_in:
+        break;
+      case LogicType::Not:
+        {
+          result = !result;
+        }
+      default:
+        break;
+    }
+    for (size_t j = 1; j < node.nfi(); j++)
+    {
+      pos = std::distance(netlist.cbegin(), std::find(netlist.cbegin(), netlist.cend(), node.fin.at(j)));
+      switch(node.type)
+      {
+        case LogicType::And:
+          result = result.And(bdd_netlist.at(pos)); break;
+        case LogicType::Nand:
+          result = result.Nand(bdd_netlist.at(pos)); break;
+        case LogicType::Or:
+          result = result.Or(bdd_netlist.at(pos)); break;
+        case LogicType::Nor:
+          result = result.Nor(bdd_netlist.at(pos)); break;
+        case LogicType::Xor:
+          result = result.Xor(bdd_netlist.at(pos)); break;
+        case LogicType::Xnor:
+          result = result.Xnor(bdd_netlist.at(pos)); break;
+        default:
+          break;
+      }
+    }
+    bdd_netlist.emplace_back(std::move(result));
   }
   for(const auto &i : pi)
   {
@@ -80,14 +73,13 @@ void BDDCircuit::to_bdd()
     bdd_pi.emplace_back(bdd_netlist.at(pos));
     // find the corresponding entry in netlist 
     // and link it. 
-
-
   }
   for(const auto &i : flops)
   {
     auto pos1 = (std::distance(netlist.begin(), std::find(netlist.begin(), netlist.end(), i.first)));
     auto pos2 = (std::distance(netlist.begin(), std::find(netlist.begin(), netlist.end(), i.second)));
     bdd_flops.emplace_back(bdd_netlist.at(pos1), bdd_netlist.at(pos2));
+    assert(!(bdd_flops.back().second.IsZero()));
   }
 
   // iterate through the rest of the netlist, ignoring DFFs and Inputs
@@ -97,6 +89,7 @@ void BDDCircuit::to_bdd()
   {
     auto pos = (std::distance(netlist.begin(), std::find(netlist.begin(), netlist.end(), i)));
     bdd_po.emplace_back(bdd_netlist.at(pos));
+    assert(!bdd_po.back().IsZero());
   }
   assert(flops.size() == bdd_flops.size());
   assert(pi.size() == bdd_pi.size());
